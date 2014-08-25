6db273f399698533445d2f6d27b3ac04ea1e5a3f
Bjoern Andres <bjoern@andres.sc>
2014-08-25 17:44:49 +0200

    improving cmake find scripts

--- cmake/modules/FindCPLEX.cmake
+++ cmake/modules/FindCPLEX.cmake
@@ -1,33 +1,40 @@
-SET(CPLEX_ROOT_DIR "" CACHE PATH "cplex root directory")
-SET(CONCERT_ROOT_DIR "" CACHE PATH "concert root directory")
+set(CPLEX_ROOT_DIR "" CACHE PATH "CPLEX root directory.")
 
-FIND_PATH(CPLEX_INCLUDE_DIR ilcplex/cplex.h HINTS ${CPLEX_ROOT_DIR}/include)
-FIND_PATH(CONCERT_INCLUDE_DIR ilconcert/iloenv.h HINTS ${CONCERT_ROOT_DIR}/include)
+find_path(CPLEX_INCLUDE_DIR ilcplex/cplex.h HINTS
+    ${CPLEX_ROOT_DIR}/cplex/include
+)
+find_path(CPLEX_CONCERT_INCLUDE_DIR ilconcert/iloenv.h HINTS
+    ${CPLEX_ROOT_DIR}/concert/include
+)
+find_library(CPLEX_LIBRARY NAMES cplex HINTS
+    ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_debian4.0_4.1/static_pic
+    ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_sles10_4.1/static_pic
+    ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic
+)
+find_library(CPLEX_ILOCPLEX_LIBRARY ilocplex HINTS
+    ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_debian4.0_4.1/static_pic
+    ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_sles10_4.1/static_pic
+    ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic
+)
+find_library(CPLEX_CONCERT_LIBRARY concert HINTS
+    ${CPLEX_ROOT_DIR}/concert/lib/x86-64_debian4.0_4.1/static_pic
+    ${CPLEX_ROOT_DIR}/concert/lib/x86-64_sles10_4.1/static_pic #unix
+    ${CPLEX_ROOT_DIR}/concert/lib/x86-64_osx/static_pic
+)
+find_path(CPLEX_BIN_DIR cplex HINTS
+    ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_sles10_4.1
+    ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_debian4.0_4.1
+    ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_osx
+)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CPLEX DEFAULT_MSG CPLEX_LIBRARY CPLEX_INCLUDE_DIR CPLEX_ILOCPLEX_LIBRARY CPLEX_CONCERT_LIBRARY CPLEX_CONCERT_INCLUDE_DIR)
 
-if(WIN32)
-	FIND_PATH(CPLEX_BIN_DIR cplex124.dll HINTS ${CPLEX_ROOT_DIR}/bin/x64_win64)
+if(CPLEX_FOUND)
+    set(CPLEX_INCLUDE_DIRS ${CPLEX_INCLUDE_DIR} ${CPLEX_CONCERT_INCLUDE_DIR})
+    set(CPLEX_LIBRARIES ${CPLEX_CONCERT_LIBRARY} ${CPLEX_ILOCPLEX_LIBRARY} ${CPLEX_LIBRARY})
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(CPLEX_LIBRARIES "${CPLEX_LIBRARIES};m;pthread")
+    endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+endif(CPLEX_FOUND)
 
-    FIND_LIBRARY(CPLEX_LIBRARY_RELEASE cplex124 HINTS ${CPLEX_ROOT_DIR}/lib/x64_windows_vs2010/stat_mda)
-    FIND_LIBRARY(CPLEX_LIBRARY_DEBUG cplex124 HINTS ${CPLEX_ROOT_DIR}/lib/x64_windows_vs2010/stat_mdd)
-    set(CPLEX_LIBRARY "optimized;${CPLEX_LIBRARY_RELEASE};debug;${CPLEX_LIBRARY_DEBUG}")
-
-    FIND_LIBRARY(ILOCPLEX_LIBRARY_RELEASE ilocplex HINTS ${CPLEX_ROOT_DIR}/lib/x64_windows_vs2010/stat_mda)
-    FIND_LIBRARY(ILOCPLEX_LIBRARY_DEBUG ilocplex HINTS ${CPLEX_ROOT_DIR}/lib/x64_windows_vs2010/stat_mdd)
-    set(ILOCPLEX_LIBRARY "optimized;${ILOCPLEX_LIBRARY_RELEASE};debug;${ILOCPLEX_LIBRARY_DEBUG}")
-
-    FIND_LIBRARY(CONCERT_LIBRARY_RELEASE concert HINTS ${CONCERT_ROOT_DIR}/lib/x64_windows_vs2010/stat_mda)
-    FIND_LIBRARY(CONCERT_LIBRARY_DEBUG concert HINTS ${CONCERT_ROOT_DIR}/lib/x64_windows_vs2010/stat_mdd)
-    set(CONCERT_LIBRARY "optimized;${CONCERT_LIBRARY_RELEASE};debug;${CONCERT_LIBRARY_DEBUG}")
-else()
-	FIND_PATH(CPLEX_BIN_DIR libcplex124.so HINTS ${CPLEX_ROOT_DIR}/bin)
-
-	# TODO: fill this in for linux
-endif()
-
-IF(CPLEX_LIBRARY AND ILOCPLEX_LIBRARY AND CONCERT_LIBRARY)
-   SET(CPLEX_FOUND TRUE)
-   MESSAGE(STATUS "Cplex found.")
-ELSE()
-   MESSAGE("Cplex NOT found.")
-ENDIF()
-
+mark_as_advanced(CPLEX_LIBRARY CPLEX_INCLUDE_DIR CPLEX_ILOCPLEX_LIBRARY CPLEX_CONCERT_INCLUDE_DIR CPLEX_CONCERT_LIBRARY)
