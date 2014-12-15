41ac619233851c456d8792a26e43579d78f62a3e
Evgeny Levinkov <levinkov@mpi-inf.mpg.de>
2014-12-15 17:30:52 +0100

    Fixed DFS. Added functionality to it

--- include/andres/graph/dfs.hxx
+++ include/andres/graph/dfs.hxx
@@ -4,34 +4,36 @@
 
 #include <cassert>
 #include <cstddef>
-#include <vector>
 #include <stack>
+#include <type_traits>
+#include <vector>
+
+#include "subgraph.hxx"
 
 namespace andres {
 namespace graph {
 
-template<class S = std::size_t>
+template<typename S = std::size_t>
 class DepthFirstSearchData {
 public:
     typedef S size_type;
 
-    DepthFirstSearchData(const size_type size = 0)
-        :   visited_(size, 0),
-            stack_()
+    DepthFirstSearchData(const size_type size)
+        :   visited_(size)
         {}
-    void assign(const size_type size = 0)
-        {
-            visited_.assign(size, 0);
-            if(!stack_.empty()) {
-                stack_ = std::stack<size_type>();
-            }
-        }
+    template<typename GRAPH>
+    DepthFirstSearchData(const GRAPH& graph)
+        :   visited_(graph.numberOfVertices())
+        {}
+
     size_type add(const size_type v)
         { stack_.push(v); }
+    void clear_stack()
+        { stack_ = std::stack<size_type>(); }
+    bool empty() const
+        { return stack_.empty(); }
     size_type next()
         { const size_type v = stack_.top(); stack_.pop(); return v; }
-    bool isEmpty() const
-        { return stack_.empty(); }
     unsigned char& visited(const size_type v)
         { return visited_[v]; }
     unsigned char visited(const size_type v) const
@@ -42,77 +44,136 @@ private:
     std::stack<size_type> stack_;
 };
 
-template<class GRAPH, class CALLBACK>
-void
+template<typename GRAPH, typename CALLBACK>
+inline void
 depthFirstSearch(
     const GRAPH& g,
-    const std::size_t startVertex,
+    CALLBACK& callback
+)
+{
+    DepthFirstSearchData<std::size_t> data(g);
+    depthFirstSearch(g, callback, data);
+}
+
+template<typename GRAPH, typename CALLBACK>
+inline void
+depthFirstSearch(
+    const GRAPH& g,
     CALLBACK& callback,
     DepthFirstSearchData<std::size_t>& data
-) {
-    typedef std::size_t size_type;
-    typedef typename GRAPH::VertexIterator VertexIterator;
-    typedef DepthFirstSearchData<std::size_t> SearchDataType;
+)
+{
+    for(std::size_t v = 0; v < g.numberOfVertices(); ++v)
+        if(!data.visited(v))
+            depthFirstSearch(g, DefaultSubgraphMask<>(), v, callback, data);
+}
 
-    assert(startVertex < g.numberOfVertices());
-    data.assign(g.numberOfVertices());
-    data.add(startVertex);
-    while(!data.isEmpty()) {
-        const size_type v = data.next();
-        if(data.visited(v) == 0) {
+template<typename GRAPH, typename CALLBACK>
+inline void
+depthFirstSearch(
+    const GRAPH& g,
+    const std::size_t start_vertex,
+    CALLBACK& callback
+)
+{
+    DepthFirstSearchData<std::size_t> data(g);
+    depthFirstSearch(g, start_vertex, callback, data);
+}
+
+template<typename GRAPH, typename CALLBACK>
+inline void
+depthFirstSearch(
+    const GRAPH& g,
+    const std::size_t start_vertex,
+    CALLBACK& callback,
+    DepthFirstSearchData<std::size_t>& data
+)
+{
+    depthFirstSearch(g, DefaultSubgraphMask<>(), start_vertex, callback, data);
+}
+
+template<typename GRAPH, typename SUBGRAPH, typename CALLBACK, typename = typename std::enable_if<std::is_class<SUBGRAPH>::value>::type>
+inline void
+depthFirstSearch(
+    const GRAPH& g,
+    const SUBGRAPH& subgraph_mask,
+    CALLBACK& callback
+)
+{
+    DepthFirstSearchData<std::size_t> data(g);
+    depthFirstSearch(g, subgraph_mask, callback, data);
+}
+
+template<typename GRAPH, typename SUBGRAPH, typename CALLBACK, typename = typename std::enable_if<std::is_class<SUBGRAPH>::value>::type>
+inline void
+depthFirstSearch(
+    const GRAPH& g,
+    const SUBGRAPH& subgraph_mask,
+    CALLBACK& callback,
+    DepthFirstSearchData<std::size_t>& data
+)
+{
+    for(std::size_t v = 0; v < g.numberOfVertices(); ++v)
+        if(!data.visited(v) && subgraph_mask.vertex(v))
+            depthFirstSearch(g, subgraph_mask, v, callback, data);
+}
+
+template<typename GRAPH, typename SUBGRAPH, typename CALLBACK>
+inline void
+depthFirstSearch(
+    const GRAPH& g,
+    const SUBGRAPH& subgraph_mask,
+    const std::size_t start_vertex,
+    CALLBACK& callback
+)
+{
+    DepthFirstSearchData<std::size_t> data(g);
+    depthFirstSearch(g, subgraph_mask, start_vertex, callback, data);
+}
+
+template<typename GRAPH, typename SUBGRAPH, typename CALLBACK>
+inline void
+depthFirstSearch(
+    const GRAPH& g,
+    const SUBGRAPH& subgraph_mask,
+    const std::size_t start_vertex,
+    CALLBACK& callback,
+    DepthFirstSearchData<std::size_t>& data
+)
+{
+    typedef std::size_t size_type;
+
+    assert(start_vertex < g.numberOfVertices());
+
+    bool proceed;
+    bool addNeighbors;
+    data.add(start_vertex);
+
+    while(!data.empty())
+    {
+        auto v = data.next();
+
+        if (!data.visited(v))
+        {
             data.visited(v) = 1;
-            bool proceed;
-            bool addNeighbors;
+            
             callback(v, proceed, addNeighbors);
-            if(!proceed) {
+            
+            if (!proceed)
+            {
+                data.clear_stack();
                 return;
             }
-            if(addNeighbors) {
-                for(VertexIterator it = g.verticesFromVertexBegin(v); it != g.verticesFromVertexEnd(v); ++it) {
-                    const size_type w = *it;
-                    data.add(w);
-                }
+            
+            if (addNeighbors)
+            {
+                auto e_it = g.edgesFromVertexBegin(v);
+                for(auto it = g.verticesFromVertexBegin(v); it != g.verticesFromVertexEnd(v); ++it, ++e_it)
+                    if (!data.visited(*it) && subgraph_mask.vertex(*it) && subgraph_mask.edge(*e_it))
+                        data.add(*it);
             }
         }
     }
-}
-
-template<class GRAPH, class CALLBACK>
-void
-depthFirstSearch(
-    const GRAPH& g,
-    CALLBACK& callback,
-    DepthFirstSearchData<std::size_t>& data
-) {
-    typedef DepthFirstSearchData<std::size_t> SearchDataType;
-
-    depthFirstSearch(g, 0, callback, data);
-    for(std::size_t v = 0; v < g.numberOfVertices(); ++v) {
-        if(data.visited(v) == 0) {
-            depthFirstSearch(g, v, callback, data);
-        }
-    }
-}
-
-template<class GRAPH, class CALLBACK>
-inline void
-depthFirstSearch(
-    const GRAPH& g,
-    const std::size_t startVertex,
-    CALLBACK& callback
-) {
-    DepthFirstSearchData<std::size_t> data;
-    depthFirstSearch(g, startVertex, callback, data);
-}
-
-template<class GRAPH, class CALLBACK>
-inline void
-depthFirstSearch(
-    const GRAPH& g,
-    CALLBACK& callback
-) {
-    DepthFirstSearchData<std::size_t> data;
-    depthFirstSearch(g, callback, data);
 }
 
 } // namespace graph
